224546350f33b8cf33da57344389f5a3541113d4
Ndi-Shema <f.shema@alustudent.com>
2024-04-11 14:43:04 +0200

    8kyu Kata - Collinearity

--- NetNinjaJs.js/object1.js
+++ NetNinjaJs.js/object1.js
@@ -1,30 +1,36 @@
 
-// const blogs = [
-//     {title: 'why mac & chesse rules', likes: 30},
-//     {title:'10 things to make with marmite', likes: 50}
-// ];
-// console.log(blogs);
+// // const blogs = [
+// //     {title: 'why mac & chesse rules', likes: 30},
+// //     {title:'10 things to make with marmite', likes: 50}
+// // ];
+// // console.log(blogs);
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'crystal@thenetninja.co.uk',
-    location: 'berlin',
-    blogs: [
-        {title: 'why mac & chesse rules', likes: 30},
-        {title:'10 things to make with marmite', likes: 50}],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logouts: function(){
-        console.log('the user logged out')
-    },
-    logBlogs: function(){
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        })
-    }
-};
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'crystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: [
+//         {title: 'why mac & chesse rules', likes: 30},
+//         {title:'10 things to make with marmite', likes: 50}],
+//     login: function(){
+//         console.log('the user logged in');
+//     },
+//     logouts: function(){
+//         console.log('the user logged out')
+//     },
+//     logBlogs: function(){
+//         console.log('this user has written the following blogs:');
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         })
+//     }
+// };
 
-user.logBlogs();
+// user.logBlogs();
+
+// Using math method to generate random numbers
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
