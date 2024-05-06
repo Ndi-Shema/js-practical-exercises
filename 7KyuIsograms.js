56bd2a93d0d9747dcd9c9d4ec149f034962f94b7
Ndi-Shema <f.shema@alustudent.com>
2024-05-06 10:34:30 +0200

    7kyu Isograms Codewars

--- 7KyuIsograms.js
+++ 7KyuIsograms.js
@@ -10,6 +10,18 @@
 
 
 
+// OTHER ALTERNATIVES
+
+// function isIsogram(str){
+//     var i, j;
+//     str = str.toLowerCase();
+//     for(i = 0; i < str.length; ++i)
+//       for(j = i + 1; j < str.length; ++j)
+//         if(str[i] === str[j])
+//           return false;
+//     return true;
+//   }
+
 
 
 
