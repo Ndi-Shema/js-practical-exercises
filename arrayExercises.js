f5d34b804592b1f687048d2d5a9ba7af05be5c40
Ndi-Shema <f.shema@alustudent.com>
2024-05-13 23:18:32 +0200

    char at odd indices

--- arrayExercises.js
+++ arrayExercises.js
@@ -25,7 +25,7 @@
 // }
 // console.log(sum([1,2,3,4]));
 function strngI(name){
-    let word ="";
+    let word =""; //store the char at the odd indices
     for (let i =0; i<name.length; i++){
         if (([i]%2) !==0) {
             word += name.charAt(i);
